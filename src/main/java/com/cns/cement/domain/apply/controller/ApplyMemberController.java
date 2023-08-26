1e2882d0c31a7b9ec5dcc51824708163577d6729
SNOW <kancth03@gmail.com>
2023-08-26 10:07:03 +0900

    Feat: 전화번호 입력창 유효성 검사 추가

--- src/main/java/com/cns/cement/domain/apply/controller/ApplyMemberController.java
+++ src/main/java/com/cns/cement/domain/apply/controller/ApplyMemberController.java
@@ -1,21 +1,21 @@
 package com.cns.cement.domain.apply.controller;
 
 import com.cns.cement.domain.apply.dto.ApplyMemberRequest;
-import com.cns.cement.domain.apply.dto.ApplyMemberResponse;
 import com.cns.cement.domain.apply.service.ApplyMemberService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 @RequiredArgsConstructor
-@RestController
+@Controller
 public class ApplyMemberController {
 
     private final ApplyMemberService applyMemberService;
 
     // form으로 데이터를 묶어서 들어오는 경우 @RequestBody 어노테이션이 없어야 정상 작동함
     @PostMapping("/register")
-    public ApplyMemberResponse applyMember(ApplyMemberRequest request) {
-        return applyMemberService.addApplyMember(request);
+    public String applyMember(ApplyMemberRequest request) {
+        applyMemberService.addApplyMember(request);
+        return "redirect:/login";
     }
 }
