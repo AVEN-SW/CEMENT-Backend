fbfd85f451fc40aa916b85647f9a4b51f6a7fa88
hyowon3434 <hyowon3434@naver.com>
2023-09-05 23:44:16 +0900

    Feat: 인스타피드 dto 생성 및 엔티티 수정

--- src/main/java/com/cns/cement/domain/InstaTaged/entity/InstaTaged.java
+++ src/main/java/com/cns/cement/domain/InstaTaged/entity/InstaTaged.java
@@ -19,12 +19,19 @@ public class InstaTaged {
     private String writer;
     private String created_At;
     private String file_url;
+    private String feed_url;
+    private String thumb_url;
+    private String data_type;
 
     @Builder
-    public InstaTaged(String writer, String file_url, String created_At){
+    public InstaTaged(String writer, String file_url, String created_At,
+                      String feed_url, String thumb_url,String data_type){
         this.writer = writer;
         this.file_url = file_url;
         this.created_At = created_At;
+        this.feed_url = feed_url;
+        this.thumb_url = thumb_url;
+        this.data_type = data_type;
     }
 
 }
